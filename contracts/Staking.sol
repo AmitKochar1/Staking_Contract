8e45df97bc4d839c87e70f4879e268aeab18bc26
Sol-Devloper <blockhunt14@gmail.com>
2023-03-09 22:01:49 +1300

    Staking contract - Adding reward functions

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -9,21 +9,33 @@ error Staking_UserAlreadyExist();
 error Staking_NotEnoughBalance();
 error Staking_UserNotStaking();
 error Staking_TxFailed();
+error Staking_NotStakedLongEnough();
 
 contract Staking is AccessControl, Pausable {
     //state variables
     uint256 public s_stakingTime;
+    uint256 totalDeposit;
 
     // variable immutable
     uint256 private immutable i_amount;
+
     // variable constant
-    uint256 private constant fixedDay = 10 days;
+    uint256 private constant firstPoolTime = 5 days;
+    uint256 private constant secondPoolTime = 10 days;
+    uint256 private constant threePoolTime = 15 days;
+    uint256 private constant poolOnePercentage = 10;
+    uint256 private constant poolTwoPercentage = 20;
+    uint256 private constant poolThreePercentage = 30;
 
     //mapping
     mapping(address => uint256) balanceOfUser;
     mapping(address => bool) stakingStatus;
+    mapping(address => uint256) timePeriod;
 
     //events
+    event Stake(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
+    event WithdrewAllFunds(address indexed account, uint256 amount);
 
     //construtor
     constructor(uint256 amount) {
@@ -34,10 +46,19 @@ contract Staking is AccessControl, Pausable {
         if (_amount < i_amount) {
             revert Staking_NotEnoughBalance();
         }
-        s_stakingTime = block.timestamp();
+        uint256 stakingStarts;
+        if (!stakingStatus[_user]) {
+            stakingStarts = block.timestamp;
+            timePeriod[_user] = stakingStarts;
+        }
         stakingStatus[_user] = true;
         balanceOfUser[_user] += _amount;
-        
+        s_stakingTime = stakingStarts;
+
+        uint256 _totalDeposit = _amount;
+        totalDeposit += _totalDeposit;
+
+        emit Stake(msg.sender, _amount);
     }
 
     function withdraw(address _user, uint256 _amount) public {
@@ -56,6 +77,11 @@ contract Staking is AccessControl, Pausable {
         if (!success) {
             revert Staking_TxFailed();
         }
+
+        uint256 _totalDeposit = _amount;
+        totalDeposit -= _totalDeposit;
+
+        emit Withdraw(msg.sender, _amount);
     }
 
     function _userExitingStaking(address _user, uint256 _amount) internal {
@@ -65,10 +91,39 @@ contract Staking is AccessControl, Pausable {
         if (!success) {
             revert Staking_TxFailed();
         }
+
+        uint256 _totalDeposit = _amount;
+        totalDeposit -= _totalDeposit;
+
+        emit WithdrewAllFunds(msg.sender, _amount);
     }
 
-    function _rewardToUser() internal {
-        if(to)
+    function poolOneReward() internal view returns (uint256 rewardPerUser) {
+        if (!stakingStatus[msg.sender]) {
+            revert Staking_UserNotStaking();
+        }
+        if (timePeriod[msg.sender] < firstPoolTime) {
+            revert Staking_NotStakedLongEnough();
+        }
+        uint256 _totalDeposit = totalDeposit;
+        uint256 _poolOneReward = (_totalDeposit * poolOnePercentage) / 100;
 
+        uint256 _userShare = balanceOfUser[msg.sender];
+        uint256 _userReward = _userShare / _totalDeposit;
+
+        rewardPerUser = _userReward * _poolOneReward;
+
+        return rewardPerUser;
     }
+
+    function poolTwoReward() internal {
+        if (!stakingStatus[msg.sender]) {
+            revert Staking_UserNotStaking();
+        }
+        if (timePeriod[msg.sender] < firstPoolTime) {
+            revert Staking_NotStakedLongEnough();
+        }
+    }
+
+    function poolThreeReward() internal {}
 }
